16aa017fab48887ed7ae44c9fafede3b93b0e50c
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-04-10 13:44:30 +0900

    MVC pattern

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ import morgan        from "morgan";
 import helmet        from "helmet";
 import cookieParser  from "cookie-parser";
 import bodyParser    from "body-parser";
-import {userRouter}  from "./router";
+import globalRouter  from "./routers/globalRouter";
+import userRouter    from "./routers/userRouter";
+import videoRouter   from "./routers/videoRouter";
+import routes        from "./routes";
 
 const app = express();
-
-/*
-* router  function
-*/
-const handleHome      = (req, res) => res.send("Hello from my house!!");
-const handleProfile   = (req, res) => res.send("You are on my profile");
 
 /*
 * middleware
@@ -22,11 +19,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev"));
 app.use(helmet());
 
+/**
+ * view engine
+ */
+app.set("view engine", "pug");
+
 /*
 * router
 */
-app.get("/"       , handleHome);
-app.get("/profile", handleProfile);
-app.use("/user"   , userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users , userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
