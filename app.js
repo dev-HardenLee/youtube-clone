e12429c3f01986c20233ad65d6dfb22d7f7d49c5
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-04-11 23:38:47 +0900

    Pug partials

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-import express       from "express";
-import morgan        from "morgan";
-import helmet        from "helmet";
-import cookieParser  from "cookie-parser";
-import bodyParser    from "body-parser";
-import globalRouter  from "./routers/globalRouter";
-import userRouter    from "./routers/userRouter";
-import videoRouter   from "./routers/videoRouter";
-import routes        from "./routes";
+import bodyParser           from "body-parser";
+import cookieParser         from "cookie-parser";
+import express              from "express";
+import globalRouter         from "./routers/globalRouter";
+import helmet               from "helmet";
+import morgan               from "morgan";
+import userRouter           from "./routers/userRouter";
+import videoRouter          from "./routers/videoRouter";
+import routes               from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
 /*
 * middleware
 */
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localsMiddleware);
 
-/**
- * view engine
- */
-app.set("view engine", "pug");
+
 
 /*
 * router
 */
-app.use(routes.home, globalRouter);
+app.use(routes.home  , globalRouter);
 app.use(routes.users , userRouter);
 app.use(routes.videos, videoRouter);
 
